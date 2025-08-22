c513a462b8ca642fc01793b104ab37987e97dc72
tee1g21 <92794120+tee1g21@users.noreply.github.com>
2025-08-22 16:14:47 +0100

    Working image to csv output

--- reader_api.py
+++ reader_api.py
@@ -1,8 +1,14 @@
 import requests
 import json
+from pprint import pprint
+import sys
+import os
 
 
-def read_receipt(image): 
+def read_receipt(filename): 
+    
+    image = f"{filename}.jpg"
+    
     receipt_ocr_endpoint = 'https://ocr.asprise.com/api/v1/receipt' 
 
     r = requests.post(receipt_ocr_endpoint, data = {
@@ -11,11 +17,34 @@ def read_receipt(image):
     'ref_no': 'Lorem Ipsum', # optional ref code 
     }, files = {"file": open(image, "rb")})
 
-    print(r.text) # result in JSON
-    return r.json()
+    with open(f"{filename}.json", "w") as f:
+        json.dump(r.json(), f) 
+        
 
-
-if __name__ == "__main__":
-    image = "example2.jpg"
+def output_to_csv(d, filename):
     
-    receipt_dict = dict(read_receipt(image))
+    
+    items = d["receipts"][0]["items"]
+
+    with open(f"{filename}.csv", "w") as f: 
+        
+        out = "description, qty, amount\n"        
+        f.write(out)
+        print(out, end="\r")
+        
+        for i in items: 
+            out = f"{i["description"]},{i["qty"]},{i["amount"]}\n"
+            f.write(out)
+            print(out, end="\r")
+        
+if __name__ == "__main__":
+    
+    filename = os.path.splitext(sys.argv[1])[0]
+    
+    if (sys.argv[2] == "T"):
+        print("Reading receipt")
+        read_receipt(filename)
+    
+    with open(f"{filename}.json") as f: 
+        d = json.load(f)
+        output_to_csv(d, filename)
